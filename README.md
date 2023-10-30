6ecad53313d698a7f8bd420b770f1d73b8254f53
James Ng <njames15@gmail.com>
2023-10-30 19:34:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quote Generator
+
+## Getting started
+
+- Copy the SSH in the repo and `git clone` + repo in your local machinine
+- Run `npm install`
+- Then, run `npm run dev`
---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+
+function Main() {
+  return (
+    <div>
+      <h1>Random Quote Generator</h1>
+      <p>
+        <em>"A man is not old until regrets take the place of dreams.</em>{" "}
+        <br />
+        -John Barrymore
+      </p>
+    </div>
+  );
+}
+
+export default Main;
