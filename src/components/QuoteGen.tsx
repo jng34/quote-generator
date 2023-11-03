4f4bac52edab9c8c7bc4e8858a9480d5bbf99256
James Ng <njames15@gmail.com>
2023-11-02 23:26:20 -0400

    implemented quote api & added css

--- src/components/QuoteGen.tsx
+++ src/components/QuoteGen.tsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
-import { Quotes, allQuotes } from "../resources/quotes";
 
 function QuoteGen() {
-  const [quotes, setQuotes] = useState<Quotes[]>(allQuotes);
-  const [index, setIndex] = useState<number>(0);
+  const [currQuote, setCurrQuote] = useState<any>({});
   const [toggle, setToggle] = useState<boolean>(false);
 
+  const api_url = "https://api.quotable.io/random";
+  async function getQuote(url: string): Promise<void> {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setCurrQuote(data);
+  }
+
   useEffect(() => {
-    setIndex(Math.floor(Math.random() * quotes.length));
+    getQuote(api_url);
   }, [toggle]);
 
   return (
     <div id="quote-box">
       <h1>Random Quote Generator</h1>
       <p id="text">
-        <em>"{quotes[index].text}"</em>
+        <em>"{currQuote.content}"</em>
       </p>
-      <p id="author">-{quotes[index].author}</p>
+      <p id="author">-{currQuote.author}</p>
+      <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
+        Twitter
+      </a>
+      {/* <a id="tweet-quote" href="twitter.com/intent/tweet">Twitter</a> */}
       <button id="new-quote" onClick={() => setToggle(!toggle)}>
         New quote
       </button>
-      <p id="tweet-quote"></p>
     </div>
   );
 }
